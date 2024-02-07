a5df0b679c64ac7f5dd6e48cc26bc0713773ff0c
Deepen Dhulla <deepen@deependhulla.com>
2024-02-07 12:52:08 +0530

    update on Wed Feb  7 12:52:08 PM IST 2024

--- 02-ollama-and-webui-setup.sh
+++ 02-ollama-and-webui-setup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-cd /opt/
 
 curl https://ollama.ai/install.sh | sh
 
 #curl -fsSL https://bun.sh/install | bash
 #source /root/.bashrc
 
-cd /opt/
-git clone https://github.com/ollama-webui/ollama-webui.git
-cd - 
+git clone https://github.com/ollama-webui/ollama-webui.git /opt/ollama-webui
+/bin/cp -p extra-files/start-main.sh /opt/ollama-webui/backend/
 cd /opt/ollama-webui
 /bin/cp -RPp example.env .env
-
-npm i
-npm run build
-npm audit fix
-cd -
+echo "Installing npm - nodejs modules"
+npm i --silent
+echo "Building using npm ..will take few min"
+npm run build --silent 1>/dev/null 2>/dev/null
+npm audit fix --silent
 cd /opt/ollama-webui/backend
 python3 -m venv env
 source env/bin/activate
-pip install -r requirements.txt -U
-/bin/cp extra-files/start-main.sh /opt/ollama-webui/backend/
+
+echo "Downloading and Installing all Python requirement packages"
+echo "Around 5.6GB ..will take few more min..."
+pip install -q wheel -U
+pip install -q -r requirements.txt -U 2>/dev/null
 /opt/ollama-webui/backend/start-main.sh
 
+echo "Done ready to use access via http://<IP_ADDRESS>:8080/"
 #or use bun
 #/root/.bun/bin/bun
