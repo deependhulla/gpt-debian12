045be61086ac3db5da084d1e9810e5cd438046ab
Deepen Dhulla <deepen@deependhulla.com>
2024-02-07 13:19:39 +0530

    update on Wed Feb  7 01:19:39 PM IST 2024

---
+++ 02-ollama-main-engine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl https://ollama.ai/install.sh | sh
+
+echo "-----------------------------";
+echo "Checking Ollama Version .."
+echo "You should see the version number. "
+echo "If you encounter a \"Segmentation fault\", "
+echo "it likely means that you are not using the latest CPU, "
+echo "or the virtual CPU does not have access to all the instructions of the main CPU"
+echo "-----------------------------";
+ollama -v
+echo "-----------------------------";
--- 03-ollama-models-downloads.sh
+++ 03-ollama-models-downloads.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-
+echo "Pulling model : tinyllama"
 ollama pull tinyllama
+echo "Pulling model : llama2"
 ollama pull llama2
+echo "Pulling model : phi"
 ollama pull phi
+echo "Pulling model : llava"
+ollama pull llava
+echo "Pulling model : mistral"
+ollama pull mistral
+echo "Pulling model : codellama"
+ollama pull codellama
+
+echo "Download all Modules now install webui and explore them."
 
